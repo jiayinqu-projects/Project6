a23dd0457f5a77f656c05e57ab9dc344d898ea1e
quxxx179 <quxxx179@MacBook-Pro-5.local>
2020-02-26 04:40:37 -0600

    Commit for project 6

---
+++ R/Week6.R
@@ -0,0 +1,26 @@
+#R Studio API Code
+library(rstudioapi)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#Data Import
+library(stringi)
+citations <- stri_read_lines("../data/citations.txt")
+citations_txt <- citations[stri_isempty(citations) == FALSE]
+length(citations) - length(citations_txt)
+
+#Data Cleaning
+library(stringr)
+library(tidyverse)
+library(magrittr)
+sample(citations_txt, 10)
+citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
+  mutate(cite = str_replace_all(cite, c("\"" = "", "\'" = "")))  %>% 
+  mutate(year = gsub("[\\(\\)]", "", str_extract(cite, "\\(?(18|19|20)[:digit:]{2}\\.?\\)?"))) %>%
+  mutate(page_start = gsub("([-?]\\d+)|(\\d*\\s)", "", str_extract(cite, "(?![^\\(\\d*\\)]*\\))(\\d+[-?]\\d+)|(\\s\\d\\d?\\d?\\s\\d+)"))) %>%
+  mutate(perf_ref = str_detect(cite, fixed("performance", ignore_case = TRUE))) %>%
+  mutate(title = gsub("\\d*[\\.\\)\\s\\,]+?", "", str_extract(cite, "\\d{4}[ab]?[\\s\\.\\,\\)]+?[A-Z][a-z][a-zA-Z\\s\\,\\?\\-\\/\\:\\&]+[\\.\\?]"))) %>%
+  mutate(cite = str_replace_all(cite, "<U+FEED>", ""))  %>% 
+  mutate(first_author = str_extract(cite, "(\\*|[:alpha:]|\\-|\\s)+[\\,\\s]?([A-Z\\.\\,\\s]+)?([A-Z\\.\\,\\s]+)?([A-Z][\\.\\,\\s]+)"))
+
+   
+
